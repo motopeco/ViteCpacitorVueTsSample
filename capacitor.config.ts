6c4a12502aae6ba7693f53a0df70b35cb6657603
motopeco <peco@pelife.jp>
2022-03-10 15:02:50 +0900

    init commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'jp.pelife.shopgame',
+  appName: 'shop',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+}
+
+export default config
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/ja'
+import quasarIconSet from 'quasar/icon-set/mdi-v6'
+import '@quasar/extras/mdi-v6/mdi-v6.css'
+import 'quasar/src/css/index.sass'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(Quasar, {
+  plugins: {},
+  lang: quasarLang,
+  iconSet: quasarIconSet,
+})
+
+app.mount('#app')
